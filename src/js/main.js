cd0f5a8325825356cab338f8130655eacbe99c1a
thoomasz <thoomaszhun@gmail.com>
2016-03-25 20:06:06 +0100

    Kisebb lett

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,9 @@ jQuery.getJSON('users', function (users) {
     console.log('users', users);
     console.log('match and more');
 
-
 });
+
+//
+function checkUser() {
+    console.log('check user');
+}
